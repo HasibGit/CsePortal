4f4dff4ac8ad73a9d052bcec170f64e92c86b09a
HasibGit <48997902+HasibGit@users.noreply.github.com>
2019-12-16 13:46:11 +0600

    some fronend added 1

--- resources/views/auth/teacher.blade.php
+++ resources/views/auth/teacher.blade.php
@@ -25,18 +25,6 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="studentid" class="col-md-4 col-form-label text-md-right">{{ __('Student ID') }}</label>
-                            <div class="col-md-6">
-                                <input
-                                class="form-control"
-                                type="text"
-                                id="studentId"
-                                placeholder="Enter Student Id"
-                              />
-                            </div>
-                            
-                          </div>
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
